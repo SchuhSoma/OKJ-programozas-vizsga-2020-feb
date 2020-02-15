d34ae3a5ab5315cbb2e850e47a2c6dd6f970e4f1
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-15 17:45:22 +0100

    Add files via upload

---
+++ Balkezesek/Balkezesek/Balkezesek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Balkezesek
+    {
+        public string Nev;
+        public string PalyaraLepes;
+        public string PalyarolLelepes;
+        public int SulyFontban;
+        public int MagassagInchben;
+
+        public Balkezesek(string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Nev = dbok[0];
+            this.PalyaraLepes = dbok[1];
+            this.PalyarolLelepes = dbok[2];
+            this.SulyFontban = int.Parse(dbok[3]);
+            this.MagassagInchben = int.Parse(dbok[4]);
+        }
+    }
+}
